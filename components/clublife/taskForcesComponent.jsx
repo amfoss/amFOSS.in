98be462ca0fb75a910c9f8102b9966082c001175
Abhinavxox <abhinavxox@gmail.com>
2024-03-23 18:21:21 +0530

    Add : Added responsiveness to clublife page

--- components/clublife/taskForcesComponent.jsx
+++ components/clublife/taskForcesComponent.jsx
@@ -4,22 +4,22 @@ import data from "@/content/clublife.json";
 const taskForcesComponent = () => {
   return (
     <>
-      <div className="bg-[#242424] mt-5 pt-40 min-h-fit">
+      <div className="md:bg-[#242424] mt-5 md:pt-40 min-h-fit">
         <div className="grid md:grid-cols-3 grid-cols-1">
           <div className="flex flex-col row-span-3">
             <h1 className="md:text-4xl text-lg tracking-[0.3em] px-5 md:text-center text-left">
               {data.taskForces.title}
             </h1>
-            <p className="text-xl text-white my-10 tracking-wide text-center px-12">
+            <p className="md:text-xl text-white my-10 tracking-wide md:text-center md:px-12 px-5">
               {data.taskForces.description}
             </p>
           </div>
           {data.taskForces.content.map((cont) => (
-            <div key={cont.title} className="text-white px-20">
-              <p className="text-3xl font-medium tracking-[0.3em] uppercase fond-bold">
+            <div key={cont.title} className="text-white md:px-20 px-5">
+              <p className="md:text-3xl font-medium tracking-[0.3em] uppercase fond-bold">
                 {cont.title}
               </p>
-              <p className="text-xl mt-10 mb-20 tracking-wide">
+              <p className="md:text-xl md:mt-10 md:mb-20 my-5 tracking-wide">
                 {cont.description}
               </p>
             </div>
