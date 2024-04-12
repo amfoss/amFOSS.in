b42ceb9a99ca5f7d87d7b842be570a1bdc3fda49
Abhinavxox <abhinavxox@gmail.com>
2024-04-13 02:15:20 +0530

    Add : Added members data

--- components/team/membersAndAlumni.jsx
+++ components/team/membersAndAlumni.jsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react";
 import Title from "@/components/ui/title";
 import MemberCard from "@/components/team/memberCard";
 import data from "@/content/team.json";
+import members from "@/content/members.json";
 import { motion } from "framer-motion";
 
 const MembersAndAlumni = ({ contentFor }) => {
-  const dataFinal = contentFor === "members" ? data.members : data.alumni;
+  const dataFinal = contentFor === "members" ? members : members;
   const [visibleMembers, setVisibleMembers] = useState(dataFinal.slice(0, 6));
   const [showAllMembers, setShowAllMembers] = useState(false);
 
@@ -47,31 +48,24 @@ const MembersAndAlumni = ({ contentFor }) => {
         <Title title={contentFor === "members" ? "MEMBERS" : "ALUMNI"} />
       </div>
       <div className="2xl:px-40 xl:px-10 px-5 my-10 grid md:grid-cols-4 grid-cols-3 lg:gap-x-20 md:gap-x-10 gap-x-3 lg:gap-y-10">
-        {visibleMembers.map((member,i) => (
+        {visibleMembers.map((member, i) => (
           <motion.div
-          key={member.name}
-          initial={{opacity:0,
-            x:-50,
-            y:-50
-          }}
-          whileInView={{opacity:1,
-            x:0,
-            y:0
-          }}
-          viewport={{once:true}}
-          transition={{duration:0.3,
-            delay:0.01 * i }}
-
-
-          >
-          <MemberCard
             key={member.name}
-            name={member.name}
-            title={member.title}
-            githubLink={member.githubLink}
-            twitterLink={member.twitterLink}
-            linkedinLink={member.linkedinLink}
-          /></motion.div>
+            initial={{ opacity: 0, x: -50, y: -50 }}
+            whileInView={{ opacity: 1, x: 0, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.3, delay: 0.01 * i }}
+          >
+            <MemberCard
+              key={member.name}
+              name={member.name}
+              title={member.title}
+              githubLink={member.githubLink}
+              twitterLink={member.twitterLink}
+              linkedinLink={member.linkedinLink}
+              imgPath={member.imgPath}
+            />
+          </motion.div>
         ))}
       </div>
       <button
