b85aabbb83fee2b610393ef721ce7e719342ddf8
KK Surendran <kksurendran95@gmail.com>
2025-10-31 15:20:40 +0530

    fix: Broken external url

--- app/(root)/team/page.jsx
+++ app/(root)/team/page.jsx
@@ -3,8 +3,8 @@ import MembersAndAlumni from "@/components/team/membersAndAlumni";
 import Contact from "../contact/page";
 import TeamHeroSection from "@/components/team/teamHeroSection"; // Client Component for the page's hero and motion logic
 
-const MEMBERS_DATA_URL = 'https://raw.githubusercontent.com/amfoss/production-data/main/members.json';
-const ALUMNI_DATA_URL = 'https://raw.githubusercontent.com/amfoss/production-data/main/alumni.json';
+const MEMBERS_DATA_URL = 'https://raw.githubusercontent.com/amfoss/member-directory/production-data/members.json';
+const ALUMNI_DATA_URL = 'https://raw.githubusercontent.com/amfoss/member-directory/production-data/alumni.json';
 async function fetchTeamData(url) {
   try {
     const response = await fetch(url);
